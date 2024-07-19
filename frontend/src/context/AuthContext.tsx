faae898d35baccb8feb164113015db5225380633
Mujtaba Mubasher <81616586+MujtabaMubasher@users.noreply.github.com>
2024-07-19 18:52:29 +0500

    Update AuthContext.tsx

--- frontend/src/context/AuthContext.tsx
+++ frontend/src/context/AuthContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, ReactNode, useContext, useEffect, useState } from "react";
+import { createContext, ReactNode, useContext,  useState } from "react";
 import {  loginUser,logoutUser,signupUser } from "../helpers/api-communicator";
 
 
