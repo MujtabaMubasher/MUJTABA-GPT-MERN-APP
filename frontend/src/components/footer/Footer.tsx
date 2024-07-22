466690fbef7cdbd6b8614ccfe75b63f305f65801
Mujtaba Mubasher <81616586+MujtabaMubasher@users.noreply.github.com>
2024-07-22 18:55:20 +0500

    Update Footer.tsx

--- frontend/src/components/footer/Footer.tsx
+++ frontend/src/components/footer/Footer.tsx
@@ -1,20 +1,22 @@
 function Footer() {
   return (
-    <footer>
+    <footer className="footer">
         <div
-          style={{
+          className="footer-div"
+         /* style={{
             width: "100%",
             minHeight:"10vh",
             maxHeight: "30vh",
             marginTop: 50
-          }} 
+          }} */
         >
           <p
            className="animation"
-           style={{
+           id="footer-p"
+           /*style={{
             fontSize: "27px",
             textAlign: "center",
-           }}
+           }}*/
           >
              Mujtaba Mubasher's Creative Innovation <span 
              style={{
